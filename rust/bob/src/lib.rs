bfb20c58d9d5954ea72c1b84e653a9f358fad2fa
hkumar <harisht@udel.edu>
2018-08-13 14:51:30 -0400

    udpate

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,16 +1,21 @@
 pub fn reply(message: &str) -> &str {
     if message.trim_right().ends_with("?") {
-        if message.replace("?", "")
-            .chars().all(|c: char| c.is_uppercase() || c.is_whitespace() ) {
+        if message
+            .replace("?", "")
+            .chars()
+            .all(|c: char| c.is_uppercase() || c.is_whitespace())
+        {
             return "Calm down, I know what I'm doing!";
         }
         return "Sure.";
-    } else if message.chars().all(|c: char| c.is_whitespace())  {
+    } else if message.chars().all(|c: char| c.is_whitespace()) {
         return "Fine. Be that way!";
-    } else if message.chars()
-        .all(|c: char| c.is_uppercase() || !c.is_alphabetic() ) {
+    } else if message
+        .chars()
+        .all(|c: char| c.is_uppercase() || !c.is_alphabetic())
+    {
         return "Whoa, chill out!";
     } else {
-        return "Whatever."
+        return "Whatever.";
     }
 }
